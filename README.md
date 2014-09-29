9922d1f3abea2368d035da57b9ce2ab495bc371d
Michele Locati <michele@locati.it>
2014-09-29 14:34:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Examples for Punic
+==================
+
+See https://github.com/punic/punic and http://punic.github.io
---
+++ setup/composer.php
@@ -0,0 +1,8 @@
+<?php
+use \Punic\Unit;
+
+// This will output `2 Millisekunden`
+echo Unit::format(2, 'millisecond', 'long', 'de');
+
+// This will output `2 ms`
+echo Unit::format(2, 'millisecond', 'short', 'en');
