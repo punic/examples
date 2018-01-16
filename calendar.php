92ef1ed6261c747ebb22c6a383586c934d1cbd03
Christian Schmidt <c960657@users.noreply.github.com>
2018-01-16 18:21:10 +0100

    More examples

--- calendar.php
+++ calendar.php
@@ -20,5 +20,15 @@ echo Calendar::getWeekdayName(1);
 // This will output `1st quarter`
 echo Calendar::getQuarterName(1);
 
+// This will output `7 März 2010`
+echo Calendar::format($dt, 'd MMMM y', 'de');
+
 // This will output `domenica 7 marzo 2010`
 echo Calendar::formatDate($dt, 'full', 'it');
+
+// This will output `7/3/2010`
+echo Calendar::formatDate($dt, '~yMd', 'da');
+
+// This will output `Mar 7 – 10, 2010`
+$dt2 = Calendar::toDateTime('2010-03-10');
+echo Calendar::formatInterval($dt, $dt2, 'yMMMd');
