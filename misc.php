2c772468ead7dca5e162b4c315959c69792c14e7
Michele Locati <michele@locati.it>
2018-02-09 17:29:40 +0100

    Add examples for Punic\Misc

--- misc.php
+++ misc.php
@@ -1,13 +1,22 @@
 <?php
-use \Punic\Misc;
+use Punic\Misc;
 
 include 'vendor/autoload.php';
 
-// This will print `One, Two`
-echo Misc::joinUnits(array('One', 'Two'), '', 'en');
+// This will print '1 yd, 1 ft, 3 in'
+echo Misc::joinUnits(array('1 yd', '1 ft', '3 in'), '', 'en');
 
-// This will print `Uno e due`
-echo Misc::joinUnits(array('Uno', 'due'), '', 'it');
+// This will print '1 yd, 1 ft und 3 in'
+echo Misc::joinUnits(array('1 yd', '1 ft', '3 in'), '', 'de');
 
-// This will print `Uno, due, e tre`
-echo Misc::joinUnits(array('Uno', 'due', 'tre'), '', 'it');
+// This will print 'One, two, and three'
+echo Misc::joinAnd(array('One', 'two', 'three'), '', 'en');
+
+// This will print 'Eins, Zwei und Drei'
+echo Misc::joinAnd(array('Eins', 'Zwei', 'Drei'), '', 'de');
+
+// This will print 'One, two, or three'
+echo Misc::joinOr(array('One', 'two', 'three'), '', 'en');
+
+// This will print 'Eins, Zwei oder Drei'
+echo Misc::joinOr(array('Eins', 'Zwei', 'Drei'), '', 'de');
